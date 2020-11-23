bc369b164dc58d124ae3031b738484710bfe9430
Kubra <kubrakidir@gmail.com>
2020-11-23 20:08:13 +0300

    json dosyanın olduğu link aktif değil

---
+++ FormJson/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FormJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Durum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tittle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.desc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.STATUS2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.last_work = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.max_time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Zaman = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Durum,
+            this.tittle,
+            this.desc,
+            this.STATUS2,
+            this.last_work,
+            this.mode,
+            this.max_time,
+            this.Zaman});
+            this.listView1.Location = new System.Drawing.Point(17, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(770, 391);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Durum
+            // 
+            this.Durum.DisplayIndex = 6;
+            this.Durum.Text = "Durum";
+            this.Durum.Width = 65;
+            // 
+            // tittle
+            // 
+            this.tittle.DisplayIndex = 0;
+            this.tittle.Text = "TITLE";
+            this.tittle.Width = 124;
+            // 
+            // desc
+            // 
+            this.desc.DisplayIndex = 1;
+            this.desc.Text = "DESC";
+            this.desc.Width = 122;
+            // 
+            // STATUS2
+            // 
+            this.STATUS2.DisplayIndex = 2;
+            this.STATUS2.Text = "STATUS";
+            this.STATUS2.Width = 104;
+            // 
+            // last_work
+            // 
+            this.last_work.DisplayIndex = 3;
+            this.last_work.Text = "LAST_WORK";
+            this.last_work.Width = 121;
+            // 
+            // mode
+            // 
+            this.mode.DisplayIndex = 4;
+            this.mode.Text = "MODE";
+            this.mode.Width = 52;
+            // 
+            // max_time
+            // 
+            this.max_time.DisplayIndex = 5;
+            this.max_time.Text = "MAX_TIME";
+            this.max_time.Width = 70;
+            // 
+            // Zaman
+            // 
+            this.Zaman.Text = "İstek Zamanı";
+            this.Zaman.Width = 106;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 435);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Durum :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(15, 450);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cevap :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(78, 435);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 18);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(79, 450);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 18);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 100000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(811, 479);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Servis Monitör";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader tittle;
+        private System.Windows.Forms.ColumnHeader desc;
+        private System.Windows.Forms.ColumnHeader STATUS2;
+        private System.Windows.Forms.ColumnHeader last_work;
+        private System.Windows.Forms.ColumnHeader mode;
+        private System.Windows.Forms.ColumnHeader max_time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ColumnHeader Durum;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader Zaman;
+    }
+}
+
---
+++ FormJson/Form1.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Runtime.Serialization.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+
+namespace FormJson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            string value1 = ConfigurationManager.AppSettings["url"];
+
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead(value1);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+
+            dynamic jsonObj = JsonConvert.DeserializeObject(s);
+            var data1 = client.DownloadString(value1);
+            JObject o = JObject.Parse(data1);
+
+            if (o["status"].ToString()=="success")
+            {
+                label7.Text = "Aktif";
+                label7.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                label7.Text = "Pasif";
+                label7.ForeColor = Color.Red;
+                timer1.Stop();
+            }
+
+            label10.Text = o["msg"].ToString();
+            bool durum = true;
+            var ekle = o["result"];
+
+            listView1.Items.Clear();
+
+            foreach (var item in ekle)
+            {
+
+                ListViewItem lvi = new ListViewItem();
+                lvi.SubItems.Clear();
+
+                lvi.SubItems.Add(item["TITLE"].ToString());
+                lvi.SubItems.Add(item["DESC"].ToString());
+                lvi.SubItems.Add(item["STATUS"].ToString());
+                lvi.SubItems.Add(item["LAST_WORK"].ToString());
+                lvi.SubItems.Add(item["MODE"].ToString());
+                lvi.SubItems.Add(item["MAX_TIME"].ToString());
+
+                if (durum.ToString() == item["STATUS"].ToString())
+                {
+                    lvi.Text = "Çalışıyor";
+                }
+                else
+                {
+                    lvi.Text = "Çalışmıyor";
+                    lvi.ForeColor = Color.Red;
+                }
+                lvi.SubItems.Add(DateTime.Now.ToString("HH:mm:ss"));
+                listView1.Items.Add(lvi);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+       
+            string value1 = ConfigurationManager.AppSettings["url"];
+
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead(value1);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+
+            dynamic jsonObj = JsonConvert.DeserializeObject(s);
+            var data1 = client.DownloadString(value1);
+            JObject o = JObject.Parse(data1);
+
+            bool durum = true;
+            var ekle = o["result"];
+
+            listView1.Items.Clear();
+
+            foreach (var item in ekle)
+            {
+
+                ListViewItem lvi = new ListViewItem();
+                lvi.SubItems.Clear();
+
+                lvi.SubItems.Add(item["TITLE"].ToString());
+                lvi.SubItems.Add(item["DESC"].ToString());
+                lvi.SubItems.Add(item["STATUS"].ToString());
+                lvi.SubItems.Add(item["LAST_WORK"].ToString());
+                lvi.SubItems.Add(item["MODE"].ToString());
+                lvi.SubItems.Add(item["MAX_TIME"].ToString());
+
+                if (durum.ToString() == item["STATUS"].ToString())
+                {
+                    lvi.Text = "Çalışıyor";
+                }
+                else
+                {
+                    lvi.Text = "Çalışmıyor";
+                    lvi.ForeColor = Color.Red;
+                }
+                lvi.SubItems.Add(DateTime.Now.ToString("HH:mm:ss"));
+                listView1.Items.Add(lvi);
+            }
+        }        
+    }
+}
